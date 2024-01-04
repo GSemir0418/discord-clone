f8394b359e584e771ab41aaee158d27e39a31f74
gsemir0418 <845217811@qq.com>
2024-01-04 10:56:38 +0800

    feat: create server modal

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import './globals.css'
 import { ClerkProvider } from '@clerk/nextjs'
 import { ThemeProvider } from '@/components/providers/theme-provider'
 import { cn } from '@/lib/utils'
+import { ModalProvider } from '@/components/providers/modal-provider'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -32,6 +33,7 @@ export default function RootLayout({
             storageKey="discord-theme"
           >
             {children}
+            <ModalProvider />
           </ThemeProvider>
         </body>
       </html>
