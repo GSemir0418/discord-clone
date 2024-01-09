aea33a65aa887a830cdf4ebbf48f1695934484e1
gsemir0418 <845217811@qq.com>
2024-01-09 09:39:24 +0800

    feat: change role of member and kick members

--- components/server/server-header.tsx
+++ components/server/server-header.tsx
@@ -54,6 +54,7 @@ export const ServerHeader: React.FC<Props> = ({
         )}
         {isAdmin && (
           <DropdownMenuItem
+            onClick={() => onOpen('members', { server })}
             className="px-3 py-2 text-sm cursor-pointer"
           >
             Manage Members
