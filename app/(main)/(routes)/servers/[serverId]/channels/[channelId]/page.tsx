4b4bb3f450d9956aaea5d13d7bc51d373d9a637b
gsemir0418 <845217811@qq.com>
2024-01-17 13:43:46 +0800

    feat: chat header and mobile adaptation

--- app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
@@ -1,10 +1,46 @@
+import { redirectToSignIn } from '@clerk/nextjs'
 import React from 'react'
+import { redirect } from 'next/navigation'
+import { currentProfile } from '@/lib/current-profile'
+import { db } from '@/lib/db'
+import { ChatHeader } from '@/components/chat/chat-header'
 
-interface Props { }
-const ChannelIdPage: React.FC<Props> = () => {
+interface Props {
+  params: {
+    serverId: string
+    channelId: string
+  }
+}
+const ChannelIdPage: React.FC<Props> = async ({
+  params,
+}) => {
+  const profile = await currentProfile()
+  if (!profile)
+    return redirectToSignIn()
+
+  const channel = await db.channel.findUnique({
+    where: {
+      id: params.channelId,
+    },
+  })
+
+  const member = await db.member.findFirst({
+    where: {
+      serverId: params.serverId,
+      profileId: profile.id,
+    },
+  })
+
+  if (!channel || !member)
+    return redirect('/')
+
   return (
-    <div>
-      ChannelIdPage
+    <div className="bg-white dark:bg-[#313338] flex flex-col h-full">
+      <ChatHeader
+        name={channel.name}
+        serverId={channel.serverId}
+        type="channel"
+      />
     </div>
   )
 }
