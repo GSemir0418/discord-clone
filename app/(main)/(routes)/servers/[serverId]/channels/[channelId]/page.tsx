b6549ac50ec06960fa89fa2872c2b9b7c34ed607
gsemir0418 <845217811@qq.com>
2024-01-29 17:34:49 +0800

    feat: ChatInput

--- app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
@@ -4,6 +4,7 @@ import { redirect } from 'next/navigation'
 import { currentProfile } from '@/lib/current-profile'
 import { db } from '@/lib/db'
 import { ChatHeader } from '@/components/chat/chat-header'
+import { ChatInput } from '@/components/chat/chat-input'
 
 interface Props {
   params: {
@@ -40,6 +41,16 @@ const ChannelIdPage: React.FC<Props> = async ({
         name={channel.name}
         serverId={channel.serverId}
         type="channel"
+      />
+      <div className="flex-1">Future Messages</div>
+      <ChatInput
+        name={channel.name}
+        type='channel'
+        apiUrl='/api/socket/messages'
+        query={{
+          channelId: channel.id,
+          serverId: channel.serverId
+        }}
       />
     </div>
   )
