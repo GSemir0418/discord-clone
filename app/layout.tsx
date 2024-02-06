c9663a95747be85e8d506f1c7f27d1d984b4e18f
gsemir0418 <845217811@qq.com>
2024-02-06 17:53:57 +0800

    fix: font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,10 @@
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
 import './globals.css'
 import { ClerkProvider } from '@clerk/nextjs'
 import { ThemeProvider } from '@/components/providers/theme-provider'
-import { cn } from '@/lib/utils'
 import { ModalProvider } from '@/components/providers/modal-provider'
 import { SocketProvider } from '@/components/providers/socket-provider'
 import { QueryProvider } from '@/components/providers/query-provider'
-
-const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Team Chat App',
@@ -23,11 +19,7 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en" suppressHydrationWarning>
-        <body className={cn(
-          inter.className,
-          'bg-white dark:bg-[#313338]',
-        )}
-        >
+        <body className="bg-white dark:bg-[#313338]">
           <ThemeProvider
             attribute="class"
             defaultTheme="dark"
